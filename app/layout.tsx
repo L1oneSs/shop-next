55a5e4dff99876c53c9e6cb849c6e2cbc42280a9
GoDZiLLaZzz <luhsm1974@gmail.com>
2024-08-08 12:06:10 +0300

    Separate the whole app to the page parts, add types and modal for pizza and the other products

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './globals.css';
-import { Providers } from './providers';
 import { Nunito } from 'next/font/google';
+
+import './globals.css';
+import { Providers } from '@/shared/components/shared/providers';
 
 const nunito = Nunito({
   subsets: ['cyrillic'],
@@ -9,22 +9,17 @@ const nunito = Nunito({
   weight: ['400', '500', '600', '700', '800', '900'],
 });
 
-export const metadata = {
-  title: 'Shop Next',
-  description: 'Buy tasty products',
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html className={nunito.variable} lang="en">
+    <html lang="en">
       <head>
         <link data-rh="true" rel="icon" href="/logo.png" />
       </head>
-      <body>
+      <body className={nunito.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
